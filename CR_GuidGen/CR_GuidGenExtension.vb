2c770e2dc00b995ab21f6474e390ee28ba19cb8c
Rory Becker <RoryBecker@gmail.com>
2015-01-06 20:15:49 +0000

    - Fixed VSIX generation.

--- CR_GuidGen/CR_GuidGenExtension.vb
+++ CR_GuidGen/CR_GuidGenExtension.vb
@@ -1,9 +1,7 @@
 Imports System.ComponentModel.Composition
 Imports DevExpress.CodeRush.Common
 
-Namespace DX_GuidGen
   <Export(GetType(IVsixPluginExtension))> _
   Public Class DX_GuidGenExtension
 	  Implements IVsixPluginExtension
   End Class
-End Namespace
